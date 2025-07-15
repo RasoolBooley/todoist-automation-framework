c5e886cab1fbf997ac08dea62db1df9752956222
rasool-booley <rasool.booley@gmail.com>
2025-07-15 14:30:44 +0200

    rebase

--- cypress/e2e/createTaskSuccess.cy.js
+++ cypress/e2e/createTaskSuccess.cy.js
@@ -3,20 +3,20 @@ import loginPageSelectors from '../support/pageObjects/homePageSelectors.js';
 
 describe('Create Project Success', () => {
   before(() => {
-    cy.createProject();
+    // cy.createProject();
   });
 
   it('Create Task Success', () => {
-    cy.getTasks();
-    cy.createTask();
-    // cy.visit('https://todoist.com/');
-    // cy.contains(loginPageSelectors.loginButton).click()
-    // cy.get(loginPageSelectors.newTabCta).invoke('removeAttr', 'target').click();
-    // cy.loginUser();
+    // cy.getTasks();
+    // cy.createTask();
+    // // cy.visit('https://todoist.com/');
+    // // cy.contains(loginPageSelectors.loginButton).click()
+    // // cy.get(loginPageSelectors.newTabCta).invoke('removeAttr', 'target').click();
+    cy.loginUser();
   })
 
   after(() => {
-      cy.deleteProject();
+      // cy.deleteProject();
   });
 })
 
